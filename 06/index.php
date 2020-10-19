b8f7c02c661191baff8e1f0709811f4b6ba04acb
tachiziro1 <tachi.ziro@gmail.com>
2020-10-20 08:57:33 +0900

    add feature2

--- 06/index.php
+++ 06/index.php
@@ -59,38 +59,49 @@ public function infomation()
     echo '車の車種'. $this->name.'、車体番号'.$this->number.'、カラー:'.$this->color.'です。';
 }
 }
-
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
+class Taxi extends Car{
+    public $pickUp;
+    public $passenger;
+public function pickUp($pickUp)
+{
+    $this->pickUp = $pickUp;
+    $this->passenger += $pickUp;
+}
+public function lower($lower)
+{
+    $this->passenger -= $lower;
+    if ($this->passenger < 0) {
+        echo '降車人数に誤りがあります';
+    }else{
+        $this->pickUp - $lower;
+        echo $lower.'人降車しました。';
+    }
+}
+public function infomation()
+{
+        echo '車の車種'. $this->name.'、車体番号'.$this->number.'、カラー:'.$this->color.'、乗車人数は'.
+    $this->pickUp .'です。';
+}
+}
+// 車の情報を表示
 echo $car1->infomation();
 echo '<br>';
+echo '<hr>';
 
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+
+// 3人乗車
+$taxi1->pickUp(3);
+
+// タクシーの情報表示を表示
+echo $taxi1->infomation();
 echo '<br>';
 
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
+// 2人降車
+$taxi1->lower(2);
 echo '<br>';
 
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
+// 一人降車
+$taxi1->lower(2);
 
